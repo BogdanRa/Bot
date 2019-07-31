75fc192d23232ae8b54322692c398b8174b49b7e
Holiday <Holiday@SOT.COM>
2019-07-31 23:17:10 +0300

    доработать проверку URL

--- TGbot.py
+++ TGbot.py
@@ -1,14 +1,37 @@
 import telebot
 bot = telebot.TeleBot('916963407:AAFWeXoD5jfDeselQXaB7fvFs9kNs7HfY1w')
 
-@bot.message_handler(content_types=['text', 'document', 'audio'])
-def get_text_messages(message):
-    if message.text == "hello":
-        bot.send_message(message.from_user.id, "Hello how can I help you?")
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "say hello")
-    else:
-        bot.send_message(message.from_user.id, "I don't... unders.. you.. w /help.")
+
+keyboard1 = telebot.types.ReplyKeyboardMarkup()
+keyboard1.row('links', 'bye')
+hey = ['gmail.com', 'prnhub.com', 'gooel.com']
+BD = []
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Hey you pull me /start', reply_markup=keyboard1)
+
+"""""
+@bot.message_handler(content_types=['text'])
+def storege_message(message):
+    #if message.text in 'http':
+    BD.append(message.text)
+    #else:
+    #    pass
+    return BD
+"""""
+
+@bot.message_handler(content_types=['text'])
+def return_links(message):
+    if 'http' or 'https' in message.text:
+        BD.append(message.text)
+
+    if message.text == "links":
+
+        bot.send_message(message.chat.id, '\n'.join(BD)+'\n')
+
+
+
 
 
 
